76012dd83594205ecaf7e52762c6df8e8ad0408b
David Houghton <dhoughton@complyoninc.com>
2011-02-15 15:52:17 -0500

    everything appears ready for a release

---
+++ lib/App/Nag/Slap.pm
@@ -0,0 +1,71 @@
+#!/usr/bin/perl
+
+# ABSTRACT: a standalone dialog widget that you can't ignore
+
+=head1 DESCRIPTION
+
+C<App::Nag::Slap> creates a little Gtk2 widget that will stay on top of
+all other windows in the center on all desktops until you press its 'OK'
+button.
+
+It is basically a dialog widget divorced from any window.
+
+The idea is to create an interruption that cannot be ignored.
+
+This module is written only to serve C<nag>. Use outside of this application at your
+own risk.
+
+=cut
+
+package App::Nag::Slap;
+
+use strict;
+use warnings;
+use Gtk2 -init;
+
+=method run
+
+The one method of C<App::Nag::Slap>, C<run> configures the widget and sets it going.
+
+=cut
+
+# creates a standalone dialog window
+sub run {
+    my ( undef, $synopsis, $t, $i ) = @_;
+
+    # make window and make it obnoxious
+    my $window = Gtk2::Window->new('toplevel');
+    $window->set_border_width(10);
+    $window->set_position('center');
+    $window->stick;
+    $window->set_keep_above(1);
+
+    # pack everything in
+    my $vbox = Gtk2::VBox->new( 0, 0 );
+    $window->add($vbox);
+    my $hbox = Gtk2::HBox->new;
+    $vbox->pack_start( $hbox, 1, 1, 10 );
+    my $icon = Gtk2::Image->new_from_file($i);
+    $icon->set_alignment( .5, .5 );
+    $hbox->pack_start( $icon, 1, 1, 10 );
+    my $text = Gtk2::Label->new($t);
+    $text->set_alignment( .5, .5 );
+    $hbox->pack_end( $text, 1, 1, 10 );
+    $window->set_title($synopsis);
+    $window->signal_connect( delete_event => \&delete_event );
+    my $button = Gtk2::Button->new_from_stock('gtk-ok');
+    $button->signal_connect( clicked => \&delete_event );
+    $button->set_size_request( 32, 32 );
+    my $quitbox = Gtk2::HBox->new( 0, 0 );
+    $quitbox->pack_start( $button, 1, 0, 0 );
+    $vbox->pack_end( $quitbox, 0, 0, 0 );
+
+    # fire it up
+    $window->show_all;
+    Gtk2->main;
+}
+
+# how to close
+sub delete_event { Gtk2->main_quit; 1 }
+
+1;
